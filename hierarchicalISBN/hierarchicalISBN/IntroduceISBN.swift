5bea17217052ffcf789d4f4533ec55703522d81e
Alberto De Ávila <=>
2016-01-01 20:23:43 +0100

    Initial version with an add button that shows a view to search a book by ISBN and perform the search with this code and construct and object with the data

---
+++ hierarchicalISBN/hierarchicalISBN/IntroduceISBN.swift
@@ -0,0 +1,13 @@
+//
+//  IntroduceISBN.swift
+//  hierarchicalISBN
+//
+//  Created by Alberto De Avila Hernandez on 1/1/16.
+//  Copyright © 2016 Alberto De Avila Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+class IntroduceISBN: UIViewController {
+
+}
